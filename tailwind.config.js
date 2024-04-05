3690467edc5948b4d296ccf2bbd1b64abc75ca4c
Mohamed Ahmedou <mahmedou500@gmail.com>
2024-04-05 18:48:41 +0100

    first kiffa

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens : {
+      'md' : '868px' ,
+      'res' : '1910px',
+      // 'ma' : '900px'
+    },
+  },
+  
+  plugins: [],
+}
